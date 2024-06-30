4571ddb77a6cf05e87d494f3f353015c7928ba43
Satyajit Samal <samalsatyajit668@gmail.com>
2024-06-30 10:02:46 +0530

    Added a new file

---
+++ 7-binary-search-algorithm/FirstAndLastPosition.java
@@ -0,0 +1,41 @@
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,7,8,8,10};
+        int target = 7;
+
+        int[] ans = searchRange(nums, target);
+        System.out.println("[" + ans[0] + "," + ans[1] + "]");
+    }
+    static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+    static int search(int[] nums, int target, boolean findSearchIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length -1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(target > nums[mid])
+                start = mid + 1;
+            else if(target < nums[mid])
+                end = mid - 1;
+            else {
+                ans = mid;
+                if(findSearchIndex)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
